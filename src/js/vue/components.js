bfc82ec730c7d43604eacf834b8e1cdbb80f0324
小滕 <xiaoteng@192.168.0.103>
2020-02-01 14:25:13 +0800

    移除echarts依赖

--- src/js/vue/components.js
+++ src/js/vue/components.js
@@ -4,5 +4,4 @@ import SearchFilter from 'components/common/search-filter';
 
 Vue.component('SubMenu', SubMenu);
 Vue.component('SearchFilter', SearchFilter);
-Vue.component('Chart', (resolve) => require(['components/common/chart'], resolve));
 Vue.component('MarkdownEditor', (resolve) => require(['components/common/markdown-editor'], resolve));

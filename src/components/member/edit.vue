a28b19c68cc8c1296bc2dea5542c140a62410f09
小滕 <xiaoteng@192.168.0.103>
2020-02-01 15:36:24 +0800

    用户编辑完善

--- src/components/member/edit.vue
+++ src/components/member/edit.vue
@@ -1,28 +1,36 @@
 <template>
   <div style="padding: 15px;">
     <Form v-width="400" mode="block" ref="form" :validOnChange="true" :showErrorTip="true" :labelWidth="110" :rules="rules" :model="user">
-    <FormItem label="头像" prop="avatar">
-      <template v-slot:label>头像</template>
-      <image-upload v-model="user.avatar" name="选择头像"></image-upload>
-    </FormItem>
-    <FormItem label="昵称" prop="nick_name">
-      <template v-slot:label>昵称</template>
-      <input type="text" v-model="user.nick_name" />
-    </FormItem>
-    <FormItem label="手机号" prop="mobile">
-      <template v-slot:label>手机号</template>
-      <input type="text" v-model="user.mobile" />
-    </FormItem>
-    <FormItem label="密码" prop="password">
-      <template v-slot:label>密码</template>
-      <input type="text" v-model="user.password" />
-      <warn text="不修改密码请不要填写"></warn>
-    </FormItem>
-    <FormItem>
-      <Button color="primary" @click="update">编辑</Button>
-      <Button @click="cancel">取消</Button>
-    </FormItem>
-  </Form>
+      <FormItem label="头像" prop="avatar">
+        <template v-slot:label>头像</template>
+        <image-upload v-model="user.avatar" name="选择头像"></image-upload>
+      </FormItem>
+      <FormItem label="昵称" prop="nick_name">
+        <template v-slot:label>昵称</template>
+        <input type="text" v-model="user.nick_name" />
+      </FormItem>
+      <FormItem label="手机号" prop="mobile">
+        <template v-slot:label>手机号</template>
+        <input type="text" v-model="user.mobile" />
+      </FormItem>
+      <FormItem label="密码" prop="password">
+        <template v-slot:label>密码</template>
+        <input type="text" v-model="user.password" />
+        <warn text="不修改密码请不要填写"></warn>
+      </FormItem>
+      <FormItem label="激活" prop="is_active">
+        <template v-slot:label>激活</template>
+        <h-switch v-model="user.is_active" :trueValue="1" :falseValue="-1"></h-switch>
+      </FormItem>
+      <FormItem label="锁定" prop="is_lock">
+        <template v-slot:label>锁定</template>
+        <h-switch v-model="user.is_lock" :trueValue="1" :falseValue="-1"></h-switch>
+      </FormItem>
+      <FormItem>
+        <Button color="primary" @click="update">编辑</Button>
+        <Button @click="cancel">取消</Button>
+      </FormItem>
+    </Form>
   </div>
 </template>
 <script>

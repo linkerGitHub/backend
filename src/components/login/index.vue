7c383b2d87adff3290d2a757ad2b17bfd5d7b246
小滕 <xiaoteng@192.168.0.103>
2020-05-23 23:12:57 +0800

    优化登录

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,7 +1,7 @@
 <style lang="less">
 @gradient-color: #3788ee;
 @bg-color: #f7f8fa;
-@title-color:#3a3a3a;
+@title-color: #3a3a3a;
 @text-color: #7e7e7e;
 @placeholder-color: #7e7e7e;
 .login-vue {
@@ -20,12 +20,12 @@
     transform: translate(-50%, -50%);
     .login-content {
       letter-spacing: 2px;
-      background: #FFF;
+      background: #fff;
       padding: 70px 30px 20px;
       box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.06);
       border-radius: 3px;
       box-sizing: border-box;
-      >div {
+      > div {
         margin: 30px 0;
         &.login-input {
           position: relative;
@@ -34,7 +34,7 @@
             color: @placeholder-color;
             top: 6px;
             font-size: 16px;
-            transition: all .2s;
+            transition: all 0.2s;
             left: 0;
             pointer-events: none;
           }
@@ -49,14 +49,15 @@
             box-shadow: inset 0 0 0 1000px #fff;
             outline: none;
             box-sizing: border-box;
-            transition: .3s;
+            transition: 0.3s;
             font-weight: 200;
             &:focus {
               border-bottom-color: @gradient-color;
               box-shadow: inset 0 0 0 1000px #fff;
             }
           }
-          input:focus + .placeholder, .placeholder.fixed{
+          input:focus + .placeholder,
+          .placeholder.fixed {
             font-size: 13px;
             top: -16px;
           }
@@ -72,14 +73,13 @@
           margin: -16px 0px 40px;
           font-weight: 200;
         }
-
       }
       > .buttonDiv {
         margin-top: 45px;
         .h-btn {
           padding: 12px 0;
           font-size: 18px;
-          opacity: .8;
+          opacity: 0.8;
           border-radius: 3px;
           background: @gradient-color;
           border-color: @gradient-color;
@@ -107,25 +107,39 @@
   <div class="login-vue">
     <div class="login-container">
       <div class="login-content">
-        <div class="login-title">MeEdu</div>
+        <div class="login-title">后台登录</div>
         <div class="login-name login-input">
-          <input type="text" name="username" v-model="login.username" autocomplete="off"/>
-          <span class="placeholder" :class="{fixed: login.username != '' && login.username != null}">邮箱</span>
+          <input type="text" name="username" v-model="login.username" autocomplete="off" />
+          <span
+            class="placeholder"
+            :class="{fixed: login.username != '' && login.username != null}"
+          >邮箱</span>
         </div>
         <div class="login-password login-input">
-          <input type="password" name="password" v-model="login.password" @keyup.enter="submit" autocomplete="off"/>
-          <span class="placeholder" :class="{fixed: login.password != '' && login.password != null}">密码</span>
+          <input
+            type="password"
+            name="password"
+            v-model="login.password"
+            @keyup.enter="submit"
+            autocomplete="off"
+          />
+          <span
+            class="placeholder"
+            :class="{fixed: login.password != '' && login.password != null}"
+          >密码</span>
         </div>
         <div class="buttonDiv">
           <Button block color="primary" size="l" @click="submit">登录</Button>
         </div>
       </div>
-      <p class="copyright"> Copyright © 2019 - <a href="https://github.com/Qsnh/meedu">MeEdu</a></p>
+      <p class="copyright">
+        Copyright © 2020 -
+        <a href="https://meedu.vip">MeEdu</a>
+      </p>
     </div>
   </div>
 </template>
 <script>
-
 import Login from 'model/login/Login';
 
 export default {
@@ -134,8 +148,6 @@ export default {
       login: Login.parse({}),
       loading: false
     };
-  },
-  mounted() {
   },
   methods: {
     submit() {

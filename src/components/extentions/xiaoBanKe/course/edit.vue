aacc7682f784c291af174ac46bc62e43c7551574
小滕 <xiaoteng@192.168.0.103>
2020-08-09 14:10:50 +0800

    added: 大小班的支持

---
+++ src/components/extentions/xiaoBanKe/course/edit.vue
@@ -0,0 +1,173 @@
+<style lang="less"></style>
+<template>
+  <div class>
+    <div class="table-basic-vue frame-page h-panel">
+      <div class="h-panel-bar">
+        <span class="h-panel-title">编辑课程</span>
+      </div>
+      <div class="h-panel-body">
+        <Form
+          mode="block"
+          ref="form"
+          :validOnChange="true"
+          :showErrorTip="true"
+          :rules="rules"
+          :model="course"
+        >
+          <FormItem label="类型" prop="type">
+            <template v-slot:label>类型</template>
+            <Select v-model="course.type" :datas="courseTypes" keyName="id" titleName="name"></Select>
+          </FormItem>
+          <FormItem label="上限人数" prop="max_people_num">
+            <template v-slot:label>上限人数</template>
+            <div class="h-input-group" v-width="200">
+              <input type="text" v-model="course.max_people_num" />
+              <span class="h-input-addon">人</span>
+            </div>
+            <warn text="小班课推荐不超过6人，大班课推荐不超过50人，直播课不限制请填写0"></warn>
+          </FormItem>
+          <FormItem label="分类" prop="category_id">
+            <template v-slot:label>分类</template>
+            <Select
+              v-model="course.category_id"
+              :datas="categories"
+              keyName="id"
+              titleName="name"
+              :filterable="true"
+            ></Select>
+          </FormItem>
+          <FormItem label="老师" prop="teacher_ids">
+            <template v-slot:label>老师</template>
+            <Select
+              v-model="course.teacher_ids"
+              :datas="teachers"
+              :multiple="true"
+              keyName="id"
+              titleName="name"
+              :filterable="true"
+            ></Select>
+          </FormItem>
+          <FormItem label="课程标题" prop="title">
+            <template v-slot:label>课程标题</template>
+            <input type="text" v-model="course.title" />
+          </FormItem>
+          <FormItem label="开课时间" prop="start_at">
+            <template v-slot:label>开课时间</template>
+            <DatePicker v-model="course.start_at" type="datetime"></DatePicker>
+          </FormItem>
+          <FormItem label="课程封面" prop="thumb">
+            <template v-slot:label>课程封面</template>
+            <image-upload v-model="course.thumb" name="课程封面"></image-upload>
+          </FormItem>
+          <FormItem label="价格" prop="charge">
+            <template v-slot:label>价格</template>
+            <div class="h-input-group" v-width="200">
+              <input type="text" v-model="course.charge" />
+              <span class="h-input-addon">元</span>
+            </div>
+          </FormItem>
+          <FormItem label="原价" prop="original_charge">
+            <template v-slot:label>原价</template>
+            <div class="h-input-group" v-width="200">
+              <input type="text" v-model="course.original_charge" />
+              <span class="h-input-addon">元</span>
+            </div>
+          </FormItem>
+          <FormItem label="简短介绍" prop="short_desc">
+            <template v-slot:label>简短介绍</template>
+            <textarea v-model="course.short_desc"></textarea>
+          </FormItem>
+          <FormItem label="详细介绍" prop="description">
+            <template v-slot:label>详细介绍</template>
+            <tinymce-editor v-model="course.original_desc"></tinymce-editor>
+          </FormItem>
+          <FormItem>
+            <Button color="primary" @click="create">保存</Button>
+          </FormItem>
+        </Form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import TinymceEditor from '../../../common/tinymce';
+
+export default {
+  props: ['id'],
+  components: {
+    TinymceEditor
+  },
+  data() {
+    return {
+      course: {
+        type: null,
+        category_id: null,
+        title: '',
+        thumb: '',
+        charge: null,
+        original_charge: null,
+        short_desc: '',
+        original_desc: '',
+        is_show: 1,
+        max_people_num: null,
+        start_at: null,
+        teacher_ids: null
+      },
+      rules: {
+        required: [
+          'start_at',
+          'teacher_ids',
+          'type',
+          'max_people_num',
+          'category_id',
+          'title',
+          'thumb',
+          'charge',
+          'original_charge',
+          'short_desc',
+          'original_desc',
+          'is_show'
+        ]
+      },
+      teachers: [],
+      categories: [],
+      courseTypes: [
+        {
+          id: 0,
+          name: '小班课'
+        },
+        {
+          id: 1,
+          name: '大班课'
+        },
+        {
+          id: 2,
+          name: '直播课'
+        }
+      ]
+    };
+  },
+  mounted() {
+    R.Extentions.xiaoBanKe.Course.Edit({ id: this.id }).then(res => {
+      this.course = res.data.data;
+      this.course.teacher_ids = res.data.teacher_ids;
+    });
+    R.Extentions.xiaoBanKe.Course.Create().then(res => {
+      this.teachers = res.data.teachers;
+      this.categories = res.data.categories;
+    });
+  },
+  methods: {
+    create() {
+      let validResult = this.$refs.form.valid();
+      if (validResult.result) {
+        let data = this.course;
+        data.render_desc = this.course.original_desc;
+        R.Extentions.xiaoBanKe.Course.Update(data).then(resp => {
+          this.$emit('success');
+        });
+      }
+    }
+  }
+};
+</script>
---
+++ src/components/extentions/xiaoBanKe/course_category/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="table-basic-vue frame-page h-panel">
+    <div class="h-panel-bar">
+      <span class="h-panel-title">课程分类</span>
+    </div>
+    <div class="h-panel-body">
+      <div class="mb-10">
+        <p-button
+          glass="h-btn h-btn-primary"
+          icon="h-icon-plus"
+          permission="addons.XiaoBanKe.course_category.store"
+          text="添加分类"
+          @click="create()"
+        ></p-button>
+      </div>
+      <Table ref="table" :loading="loading" :datas="datas">
+        <TableItem prop="name" title="分类名" treeOpener></TableItem>
+        <TableItem prop="sort" title="升序"></TableItem>
+        <TableItem title="操作" align="center" :width="200">
+          <template slot-scope="{ data }">
+            <p-del-button
+              permission="addons.XiaoBanKe.course_category.delete"
+              @click="remove(datas, data)"
+            ></p-del-button>
+            <p-button
+              glass="h-btn h-btn-s h-btn-primary"
+              permission="addons.XiaoBanKe.course_category.edit"
+              text="编辑"
+              @click="edit(data)"
+            ></p-button>
+          </template>
+        </TableItem>
+      </Table>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      datas: [],
+      loading: false
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    changePage() {
+      this.getData();
+    },
+    getData() {
+      this.loading = true;
+      R.Extentions.xiaoBanKe.CourseCategory.List(this.pagination).then(resp => {
+        this.datas = resp.data.data.data;
+        this.loading = false;
+      });
+    },
+    create() {
+      this.$Modal({
+        hasCloseIcon: true,
+        closeOnMask: false,
+        component: {
+          vue: resolve => {
+            require(['./create'], resolve);
+          }
+        },
+        events: {
+          success: (modal, data) => {
+            modal.close();
+            HeyUI.$Message.success('成功');
+            this.getData(true);
+          }
+        }
+      });
+    },
+    remove(data, item) {
+      R.Extentions.xiaoBanKe.CourseCategory.Delete({ id: item.id }).then(resp => {
+        HeyUI.$Message.success('成功');
+        this.getData(true);
+      });
+    },
+    edit(item) {
+      this.$Modal({
+        hasCloseIcon: true,
+        closeOnMask: false,
+        component: {
+          vue: resolve => {
+            require(['./edit'], resolve);
+          },
+          datas: {
+            id: item.id
+          }
+        },
+        events: {
+          success: (modal, data) => {
+            modal.close();
+            HeyUI.$Message.success('成功');
+            this.getData(true);
+          }
+        }
+      });
+    }
+  }
+};
+</script>

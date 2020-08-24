59df912be56a4ca9f9b2ff4f1f8975852ae5b87d
小滕 <xiaoteng@192.168.0.103>
2020-08-24 17:07:27 +0800

    优化秒杀订单界面

--- src/components/extentions/miaosha/orders/index.vue
+++ src/components/extentions/miaosha/orders/index.vue
@@ -6,54 +6,71 @@
 <template>
   <div class="table-basic-vue frame-page h-panel">
     <div class="h-panel-bar">
-      <span class="h-panel-title">秒杀订单</span>
+      <span class="h-panel-title">订单</span>
     </div>
     <div class="h-panel-body">
-      <Form :labelWidth="110">
-        <FormItem>
-          <template v-slot:label>课程</template>
-          <Select
-            v-model="filter.gid"
-            :filterable="true"
-            :datas="goods"
-            keyName="id"
-            titleName="goods_title"
-          ></Select>
-        </FormItem>
-        <FormItem>
-          <template v-slot:label>状态</template>
-          <Select
-            v-model="filter.status"
-            :filterable="true"
-            :datas="statusMap"
-            keyName="id"
-            titleName="text"
-          ></Select>
-        </FormItem>
-        <FormItem>
-          <Button color="primary" @click="getData(true)">搜索</Button>
-          <Button class="h-btn" @click="reset">重置</Button>
-        </FormItem>
+      <Form>
+        <Row :space="10">
+          <Cell :width="6">
+            <FormItem label="课程">
+              <Select
+                v-model="filter.type"
+                :filterable="true"
+                :datas="types"
+                keyName="goods_type"
+                titleName="goods_type_text"
+              ></Select>
+            </FormItem>
+          </Cell>
+          <Cell :width="6">
+            <FormItem label="课程">
+              <Select
+                v-model="filter.gid"
+                :filterable="true"
+                :datas="goodsList"
+                keyName="id"
+                titleName="goods_title"
+              ></Select>
+            </FormItem>
+          </Cell>
+          <Cell :width="6">
+            <FormItem label="支付状态">
+              <Select
+                v-model="filter.status"
+                :filterable="true"
+                :datas="statusMap"
+                keyName="id"
+                titleName="text"
+              ></Select>
+            </FormItem>
+          </Cell>
+          <Cell :width="6">
+            <FormItem>
+              <Button color="primary" @click="getData(true)">搜索</Button>
+              <Button class="h-btn" @click="reset">重置</Button>
+            </FormItem>
+          </Cell>
+        </Row>
       </Form>
 
       <Table :loading="loading" :datas="datas">
-        <TableItem prop="id" title="ID"></TableItem>
+        <TableItem prop="id" title="ID" :width="80"></TableItem>
         <TableItem title="商品">
           <template slot-scope="{ data }">{{data.goods.goods_title}}</template>
         </TableItem>
-        <TableItem title="用户">
-          <template slot-scope="{ data }">
-            <span v-if="data.user">{{data.user.nick_name}}</span>
-            <span class="red" v-else>用户不存在</span>
-          </template>
-        </TableItem>
-        <TableItem title="价格">
+        <TableItem title="价格" :width="80">
           <template slot-scope="{ data }">{{data.charge}}元</template>
         </TableItem>
-        <TableItem title="状态">
+        <TableItem title="用户" :width="120">
+          <template slot-scope="{ data }">
+            <span v-if="data.user">{{data.user.nick_name}}</span>
+            <span class="red" v-else>已删除</span>
+          </template>
+        </TableItem>
+        <TableItem title="状态" :width="80">
           <template slot-scope="{ data }">
             <span v-if="data.status === 1">已支付</span>
-            <span v-else>未支付</span>
+            <span class="red" v-else>未支付</span>
           </template>
         </TableItem>
       </Table>
@@ -80,6 +97,7 @@ export default {
       datas: [],
       loading: false,
       filter: {
+        type: null,
         gid: null,
         status: -1
       },
@@ -97,11 +115,27 @@ export default {
           text: '已支付'
         }
       ],
-      goods: []
+      goods: [],
+      types: []
     };
   },
   mounted() {
     this.init();
+  },
+  computed: {
+    goodsList() {
+      if (!this.filter.type) {
+        return [];
+      }
+      let data = [];
+      for (let i = 0; i < this.goods.length; i++) {
+        if (this.goods[i].goods_type === this.filter.type) {
+          data.push(this.goods[i]);
+        }
+      }
+
+      return data;
+    }
   },
   methods: {
     reset() {
@@ -122,14 +156,13 @@ export default {
         this.pagination.page = 1;
       }
       this.loading = true;
-      let data = this.pagination;
-      data.gid = this.filter.gid;
-      data.status = this.filter.status;
+      let data = Object.assign(this.filter, this.pagination);
       R.Extentions.miaoSha.Orders.Index(data).then(resp => {
         this.datas = resp.data.data.data;
         this.pagination.total = resp.data.data.total;
         this.goods = resp.data.goods;
         this.loading = false;
+        this.types = resp.data.types;
       });
     }
   }

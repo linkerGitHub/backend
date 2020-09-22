7e22515fe82cfaff9156f303f09f57f988311456
小滕 <xiaoteng@192.168.0.103>
2020-09-22 22:14:50 +0800

    优化问答分类

--- src/components/extentions/wenda/category/create.vue
+++ src/components/extentions/wenda/category/create.vue
@@ -1,26 +1,36 @@
 <template>
-  <div style="padding: 15px;">
-    <Form
-      v-width="400"
-      mode="block"
-      ref="form"
-      :validOnChange="true"
-      :showErrorTip="true"
-      :rules="rules"
-      :model="category"
-    >
-      <FormItem label="分类名" prop="name">
-        <template v-slot:label>分类名</template>
-        <input type="text" v-model="category.name" />
-      </FormItem>
-      <FormItem label="排序" prop="sort">
-        <template v-slot:label>排序</template>
-        <input type="number" v-model="category.sort" />
-      </FormItem>
-      <FormItem>
-        <Button color="primary" @click="create">添加</Button>
-      </FormItem>
-    </Form>
+  <div class="h-panel w-1000">
+    <div class="h-panel-bar">
+      <span class="h-panel-title">添加</span>
+    </div>
+    <div class="h-panel-body">
+      <Form
+        mode="block"
+        ref="form"
+        :validOnChange="true"
+        :showErrorTip="true"
+        :rules="rules"
+        :model="category"
+      >
+        <Row :space="10">
+          <Cell :width="12">
+            <FormItem label="分类名" prop="name">
+              <input type="text" v-model="category.name" />
+            </FormItem>
+          </Cell>
+
+          <Cell :width="12">
+            <FormItem label="排序" prop="sort">
+              <input type="number" v-model="category.sort" />
+            </FormItem>
+          </Cell>
+        </Row>
+
+        <FormItem>
+          <Button color="primary" @click="create">添加</Button>
+        </FormItem>
+      </Form>
+    </div>
   </div>
 </template>
 <script>
--- src/components/extentions/wenda/category/index.vue
+++ src/components/extentions/wenda/category/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="table-basic-vue frame-page h-panel">
+  <div class="h-panel w-1000">
     <div class="h-panel-bar">
-      <span class="h-panel-title">课程分类</span>
+      <span class="h-panel-title">分类</span>
     </div>
     <div class="h-panel-body">
-      <div class="mb-10">
+      <div class="float-box mb-10">
         <p-button
           glass="h-btn h-btn-primary"
           icon="h-icon-plus"
@@ -13,21 +13,23 @@
           @click="create()"
         ></p-button>
       </div>
-      <Table ref="table" :loading="loading" :datas="datas">
-        <TableItem prop="name" title="分类名"></TableItem>
-        <TableItem prop="sort" title="升序"></TableItem>
-        <TableItem title="操作" align="center" :width="200">
-          <template slot-scope="{ data }">
-            <p-del-button permission="addons.Wenda.category.delete" @click="remove(datas, data)"></p-del-button>
-            <p-button
-              glass="h-btn h-btn-s h-btn-primary"
-              permission="addons.Wenda.category.update"
-              text="编辑"
-              @click="edit(data)"
-            ></p-button>
-          </template>
-        </TableItem>
-      </Table>
+      <div class="float-box mb-10">
+        <Table ref="table" :loading="loading" :datas="datas">
+          <TableItem prop="name" title="分类名"></TableItem>
+          <TableItem prop="sort" title="升序"></TableItem>
+          <TableItem title="操作" align="center" :width="200">
+            <template slot-scope="{ data }">
+              <p-del-button permission="addons.Wenda.category.delete" @click="remove(datas, data)"></p-del-button>
+              <p-button
+                glass="h-btn h-btn-s h-btn-primary"
+                permission="addons.Wenda.category.update"
+                text="编辑"
+                @click="edit(data)"
+              ></p-button>
+            </template>
+          </TableItem>
+        </Table>
+      </div>
     </div>
   </div>
 </template>
